89c4d60e5c50435053f49b750e0271d9f188126b
Mahlon Scott <1kingjms@gmail.com>
2021-03-24 23:26:25 -0400

    Added basic testing, got Compound ready for redox

--- ChemicalElements.py
+++ ChemicalElements.py
@@ -36,5 +36,13 @@ for element in __rawElements:
     value: Element = {tag: info for tag, info in element.items() if tag in Element.__annotations__}
     periodic_table[key] = value
 
+
+def test(verbose=True):
+    assert periodic_table['H'] == {'name': 'Hydrogen', 'atomic_mass': 1.008, 'boil': 20.271, 'category': 'diatomic nonmetal', 'color': None, 'density': 0.08988, 'melt': 13.99, 'molar_heat': 28.836, 'number': 1, 'period': 1, 'phase': 'Gas', 'spectral_img': 'https://en.wikipedia.org/wiki/File:Hydrogen_Spectra.jpg', 'symbol': 'H', 'xpos': 1, 'ypos': 1, 'shells': [1], 'electron_configuration': '1s1', 'electron_configuration_semantic': '1s1', 'electron_affinity': 72.769, 'electronegativity_pauling': 2.2, 'ionization_energies': [1312]}
+    assert periodic_table['U'] == {'name': 'Uranium', 'atomic_mass': 238.028913, 'boil': 4404, 'category': 'actinide', 'color': None, 'density': 19.1, 'melt': 1405.3, 'molar_heat': 27.665, 'number': 92, 'period': 7, 'phase': 'Solid', 'spectral_img': None, 'symbol': 'U', 'xpos': 6, 'ypos': 10, 'shells': [2, 8, 18, 32, 21, 9, 2], 'electron_configuration': '1s2 2s2 2p6 3s2 3p6 4s2 3d10 4p6 5s2 4d10 5p6 6s2 4f14 5d10 6p6 7s2 5f3 6d1', 'electron_configuration_semantic': '[Rn] 5f3 6d1 7s2', 'electron_affinity': 50.94, 'electronegativity_pauling': 1.38, 'ionization_energies': [597.6, 1420]}
+    if verbose:
+        print("All tests passed for Chemical Elements.")
+
+
 if __name__ == "__main__":
-    print(periodic_table['H'])
+    test()
--- Compound.py
+++ Compound.py
@@ -4,13 +4,65 @@ _nums = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
 
 class Compound(dict):
-    def __init__(self, formula):
+    def __init__(self, formula, /, charge=None):
         dict.__init__(self)
-        constitutents = findall(r"[A-Z][a-z]*[0-9]*", formula)
-        for constituent in constitutents:
-            found = split(r"(?<=[a-zA-Z])(?=[0-9])", constituent)
-            if len(found) == 1:
-                self[constituent] = 1
+
+        if charge is not None:
+            self.charge = charge
+        else:
+            self.charge = 0
+
+        if type(formula) == str:
+            constitutents = findall(r"[A-Z][a-z]*[0-9]*", formula)
+            charge = findall(r"[−\-+][0-9]*", formula)
+            for constituent in constitutents:
+                found = split(r"(?<=[a-zA-Z])(?=[0-9])", constituent)
+                if len(found) == 1:
+                    if constituent not in self:
+                        self[constituent] = 1
+                    else:
+                        self[constituent] += 1
+                else:
+                    symbol, amount = found
+                    if symbol not in self:
+                        self[symbol] = int(amount)
+                    else:
+                        self[symbol] += int(amount)
+            if charge:
+                charge = charge[0]
+                if len(charge) > 1:
+                    if charge[0] == "−" or charge[0] == "-":
+                        self.charge = int(charge[1:]) * -1
+                    else:
+                        self.charge = int(charge[1:])
+                else:
+                    if charge[0] == "−" or charge[0] == "-":
+                        self.charge = -1
+                    else:
+                        self.charge = 1
+
+        elif type(formula) == dict:
+            for key in formula:
+                self[key] = formula[key]
+
+    def __eq__(self, other):
+        if type(other) == Compound:
+            if dict.__eq__(self, other) and self.charge == other.charge:
+                return True
             else:
-                symbol, amount = found
-                self[symbol] = int(amount)
+                return False
+        else:
+            return dict.__eq__(self, other)
+
+
+def test(verbose=True):
+    assert Compound("C6H12O6") == Compound({'C': 6, 'H': 12, 'O': 6}, charge=0)
+    assert Compound("HCOF") == Compound({'H': 1, 'C': 1, 'O': 1, 'F': 1}, charge=0)
+    assert Compound("2NH3OH+") == Compound({"N": 1, "H": 4, "O": 1}, charge=1)
+    assert Compound("2NH3O2H2-2") == Compound({"N": 1, "H": 5, "O": 2}, charge=-2)
+    if verbose:
+        print("All tests passed for Compound")
+
+
+if __name__ == "__main__":
+    test()
